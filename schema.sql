85c159303aae489f5e5d25df4fb1e78af7e2f4a0
paskab012 <pascalkabikampan@gmail.com>
2022-05-06 13:01:41 +0200

    Implement changes requested by the reviewer

--- schema.sql
+++ schema.sql
@@ -1,7 +1,8 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    name varchar(50),
+    id INT NOT NULL PRIMARY KEY,
+    name VARCHAR(50),
     date_of_birth DATE,
     escape_attemps INT,
     neutered BOOLEAN,
