1f5964dbc66d359c4e0260c977417738e51b7df8
paskab012 <pascalkabikampan@gmail.com>
2022-05-06 10:33:28 +0200

    Add schemas and queries into the project

--- schema.sql
+++ schema.sql
@@ -1,5 +1,9 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    name varchar(100)
+    name varchar(50),
+    date_of_birth DATE,
+    escape_attemps INT,
+    neutered BOOLEAN,
+    weight_kg DECIMAL 
 );
